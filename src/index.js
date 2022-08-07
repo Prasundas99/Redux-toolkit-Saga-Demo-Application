60c6e0f19a5f48ad574b5810700657f986a26268
Prasundas99 <prasundas115@gmail.com>
2022-08-07 18:17:58 +0530

    store initialised with saga

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './App.css';
+
+import { Provider } from 'react-redux';
+import createSagaMiddleware from '@redux-saga/core';
+import { configureStore } from '@reduxjs/toolkit';
+
+const saga = createSagaMiddleware();
+const store = configureStore({
+    reducer: {},
+    middleware: [saga]
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 
